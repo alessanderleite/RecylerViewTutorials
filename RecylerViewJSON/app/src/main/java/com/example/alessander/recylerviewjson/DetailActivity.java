f6a76647efe2b2754c501399880a12e67e7df914
alessanderleite <alessander.logistica@gmail.com>
2018-10-12 13:26:50 -0300

    intent

--- RecylerViewJSON/app/src/main/java/com/example/alessander/recylerviewjson/DetailActivity.java
+++ RecylerViewJSON/app/src/main/java/com/example/alessander/recylerviewjson/DetailActivity.java
@@ -1,7 +1,16 @@
 package com.example.alessander.recylerviewjson;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import static com.example.alessander.recylerviewjson.MainActivity.EXTRA_CREATOR;
+import static com.example.alessander.recylerviewjson.MainActivity.EXTRA_LIKES;
+import static com.example.alessander.recylerviewjson.MainActivity.EXTRA_URL;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -9,5 +18,18 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        Intent intent = getIntent();
+        String imageUrl = intent.getStringExtra(EXTRA_URL);
+        String creatorName = intent.getStringExtra(EXTRA_CREATOR);
+        int likeCount = intent.getIntExtra(EXTRA_LIKES, 0);
+
+        ImageView imageView = findViewById(R.id.image_view_detail);
+        TextView textViewCreator = findViewById(R.id.text_view_creator_detail);
+        TextView textViewLikes = findViewById(R.id.text_view_like_detail);
+
+        Picasso.with(this).load(imageUrl).fit().centerInside().into(imageView);
+        textViewCreator.setText(creatorName);
+        textViewLikes.setText("Likes: " + likeCount);
     }
 }
