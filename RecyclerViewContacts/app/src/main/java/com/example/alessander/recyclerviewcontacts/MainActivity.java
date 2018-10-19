a7bfde6b789f7b17aaee4bc0dd79cece084e512f
alessanderleite <alessander.logistica@gmail.com>
2018-10-18 22:58:25 -0300

    declaring RecyclerView into .xml file

--- RecyclerViewContacts/app/src/main/java/com/example/alessander/recyclerviewcontacts/MainActivity.java
+++ RecyclerViewContacts/app/src/main/java/com/example/alessander/recyclerviewcontacts/MainActivity.java
@@ -2,12 +2,32 @@ package com.example.alessander.recyclerviewcontacts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> mContacts;
+    RecyclerView mRecyclerView;
+    RecyclerView.LayoutManager mLayoutManager;
+    RecyclerView.Adapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+        mContacts = new ArrayList<>();
+        mContacts.add("Alessander Leite");
+
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mAdapter = new MainAdapter(mContacts);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
     }
 }
